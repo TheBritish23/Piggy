f29da48341a78ad055bd3c391305fcfa14dc2b94
Soro Cat <sorousha326@gmail.com>
2019-10-15 10:26:27 -0700

    begin js

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// Business logic for Rules
+
+// Business logic for Game
+
+// User interface logic
+$(document).ready(function() {
+  $("form#hog").submit(function(event) {
+    event.preventDefault()
